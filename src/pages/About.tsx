9cabfaa6e7e2ce1b22dd2c365aa677896a22e78c
zidaneho <zidaneho4567@gmail.com>
2025-10-06 23:39:18 -0700

    fixed images

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,12 @@
 import NavBar from "../components/NavBar";
+import myselfLoLPark from "../assets/myself-LoLPark.png"
 
 function About() {
   return (
     <div className="flex flex-col w-full h-full mt-4 items-center">
       <NavBar includeHome={true}></NavBar>
       <div className="flex w-[90%] p-10 gap-12">
-        <img src="../src/assets/myself-LoLPark.png" className="w-80"></img>
+        <img src={myselfLoLPark} className="w-80"></img>
         <div className="flex flex-col gap-4">
           <p className="text-xl">Hello!</p>
           <p className="text-lg">My name is Zidane Ho and I'm currently studying for my B.S of Computer Science at UC San Diego. On my spare time, you'd find me building personal coding projects, going out to eat delicious food, or playing videogames with friends. I enjoy working with other like-minded people, so please feel free to contact me.</p>
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import samiraBackground from "../assets/samira_background5.jpg";
+import autobattler from "../assets/autobattler.png";
+import steamanalysis from "../assets/steamanalysis.png";
+import lab30tritonware from "../assets/lab30tritonware.png";
+import lunarlearning from "../assets/lunarlearning.png";
 
 interface Project {
   title: string;
@@ -13,35 +18,35 @@ const projects: Project[] = [
     title: "SamiraMod: A Risk of Rain 2 Mod",
     description:
       "Engineered and launched a character mod, achieving over 149,000 downloads on Thunderstore.io. Developed custom character abilities, animations, and gameplay mechanics using Risk of Rain 2's API and C#. Iteratively improved the mod by actively managing user feedback and resolving issues via the project's GitHub repository.",
-    image: "../src/assets/samira_background5.jpg",
+    image: samiraBackground,
     repoLink: "https://thunderstore.io/c/risk-of-rain-2/p/CafeHo/SamiraMod/",
   },
   {
     title: "Auto Battler Game Engine",
     description:
       "An auto battler roguelike. Field units, level them up, equip new items, evolve them, fight challenging enemies, on various terrains. This is a solo project that is currently a work in progress.",
-    image: "../src/assets/autobattler.png",
+    image: autobattler,
     repoLink: "https://github.com/zidaneho/auto-battler",
   },
   {
     title: "Steam Review Analysis Tool",
     description:
       "Created a full-stack research tool to provide game developers with competitive analysis of similar games on Steam. Utilized cosine similarity on review embeddings to identify and rank analogous games for market research and integrated the Gemini API to perform sentiment analysis and generate concise summaries of thousands of user reviews.",
-    image: "../src/assets/steamanalysis.png",
+    image: steamanalysis,
     repoLink: "https://github.com/zidaneho/steam_analysis",
   },
   {
     title: "Lab 30",
     description:
       "Find yourself immersed in a dark lab full of dangerous monsters. Avoid them and find a way out of the lab! This project was apart of UCSD VGDC's Tritonware kickoff in Fall 2024, made by 5 members to design and deliver the project within 2 weeks.",
-    image: "../src/assets/lab30tritonware.png",
+    image: lab30tritonware,
     repoLink: "https://github.com/zidaneho/TritonWare30",
   },
   {
     title: "Lunar Learning",
     description:
       "Lunar Learning is an interactive, space-themed science game designed specifically to engage elementary school students with learning disabilities. This project is apart of UCSD ACM's DiamondHacks 2025, created by a team of 4. I helped contribute to this project by designing the UI and page.",
-    image: "../src/assets/lunarlearning.png",
+    image: lunarlearning,
     repoLink: "https://github.com/zidaneho/mission_learn",
   },
 ];
