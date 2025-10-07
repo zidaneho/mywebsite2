c549dbfa48c7c80aac40df9b2b5015d1864d553a
zidaneho <zidaneho4567@gmail.com>
2025-10-06 22:40:11 -0700

    finished website base

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import profilePicture from "../assets/profile-picture.png";
-import collegePicture from "../assets/college.png";
-import homePicture from "../assets/home.png";
+import { GraduationCap, House } from "lucide-react";
 const Profile: React.FC = () => {
   return (
     <div className="flex flex-row">
-      <img
-        src={profilePicture}
-        className="w-48 h-48 rounded-full object-cover"
-      ></img>
-      <div className="flex flex-col pl-4">
+      <div className="w-48 h-48 rounded-full overflow-hidden flex-shrink-0">
+        <img
+          src={profilePicture}
+          className="w-full h-full object-cover"
+          alt="Zidane Ho"
+        />
+      </div>
+      <div className="flex flex-col pl-4 animate-fade-in-slide [animation-delay:50ms]">
         <p className="font-extrabold text-3xl">Zidane Ho</p>
-        <p className="text-sm font-medium">Games Developer</p>
+        <p className="text-sm font-medium">Game Developer</p>
         <div className="flex flex-row items-center mt-1 gap-2">
-          <img src={collegePicture} className="w-8 h-8"></img>
-          <p className="text-md text-gray">Computer Science at UC San Diego</p>
+          <GraduationCap className="w-4 h-4"></GraduationCap>
+          <p className="text-sm">B.S of Computer Science at UC San Diego</p>
         </div>
         <div className="flex flex-row items-center mt-1 gap-2">
-          <img src={homePicture} className="w-8 h-8"></img>
-          <p className="text-md text-gray">Saratoga, CA</p>
+          <House className="w-4 h-4"></House>
+          <p className="text-sm">Saratoga, CA</p>
         </div>
       </div>
     </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,15 @@ import NavBar from "../components/NavBar";
 
 function About() {
   return (
-    <div className="flex flex-row justify-center w-full h-full mt-4">
+    <div className="flex flex-col w-full h-full mt-4 items-center">
       <NavBar includeHome={true}></NavBar>
+      <div className="flex w-[90%] p-10 gap-12">
+        <img src="../src/assets/myself-LoLPark.png" className="w-80"></img>
+        <div className="flex flex-col gap-4">
+          <p className="text-xl">Hello!</p>
+          <p className="text-lg">My name is Zidane Ho and I'm currently studying for my B.S of Computer Science at UC San Diego. On my spare time, you'd find me building personal coding projects, going out to eat delicious food, or playing videogames with friends. I enjoy working with other like-minded people, so please feel free to contact me.</p>
+        </div>
+      </div>
     </div>
   );
 }
